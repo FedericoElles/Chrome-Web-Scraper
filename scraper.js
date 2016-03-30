03b81f7ef51fe63206d83ca3399a71922e468fa2
Federico Elles <FedericoElles@users.noreply.github.com>
2016-03-30 15:40:17 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,6 +7,15 @@
 	//store map of visited pages
 	var data = {};
 
+	function loadScript(url, callback)
+	{
+	    var script = document.createElement('script');
+	    script.type = 'text/javascript';
+	    script.src = url;
+	    script.onreadystatechange = callback;
+	    script.onload = callback;
+	    document.getElementsByTagName('head')[0].appendChild(script);
+	}
 
 	function logx(txt){
 		console.log('SCRAPER> ' + txt);
